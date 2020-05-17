bd1ba14ff841568d202a0e2428611df93b20dabf
sathvik <sathvik@tatvait.com>
2020-05-17 14:01:54 +0530

    First Commit   added express server   added dockerfile

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) => res.send('Hello World Hii'));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port,()=> {
+    console.log(`Server running on port ${port}`);
+});
